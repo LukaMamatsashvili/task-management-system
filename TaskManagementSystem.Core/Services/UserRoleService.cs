faa7baa38bef7592601c26f81d477aedcb88c5f2
Luka Mamatsashvili <87601863+LukaMamatsashvili@users.noreply.github.com>
2023-04-23 23:03:09 +0400

    Add same for service classes

--- TaskManagementSystem.Core/Services/UserRoleService.cs
+++ TaskManagementSystem.Core/Services/UserRoleService.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TaskManagementSystem.Core.Interfaces;
+using TaskManagementSystem.Infrastructure.Models;
+using Task = System.Threading.Tasks.Task;
 
 namespace TaskManagementSystem.Core.Services
 {
