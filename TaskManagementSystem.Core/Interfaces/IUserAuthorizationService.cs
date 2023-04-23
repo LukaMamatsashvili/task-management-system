9062d2d0bd0216964270dc84054dc81a13bd59fd
Luka Mamatsashvili <87601863+LukaMamatsashvili@users.noreply.github.com>
2023-04-23 23:01:00 +0400

    Import neccessary objects

--- TaskManagementSystem.Core/Interfaces/IUserAuthorizationService.cs
+++ TaskManagementSystem.Core/Interfaces/IUserAuthorizationService.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TaskManagementSystem.Infrastructure.Models;
+using Task = System.Threading.Tasks.Task;
 
 namespace TaskManagementSystem.Core.Interfaces
 {
